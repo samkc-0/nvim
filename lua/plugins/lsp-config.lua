59b276459bd5cf21402804c35295c4edf5f7a29a
samkc-0 <sam.k.c.0@icloud.com>
2024-06-22 22:57:47 +1200

    enable code_actions in lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,8 +36,8 @@ return {
       lspconfig.pyright.setup({})
       lspconfig.tsserver.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.bug.definition, {})
-      vim.keymap.set({ "n", "v", }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
