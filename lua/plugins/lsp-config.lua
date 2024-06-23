d7f0923fb2296d8e295ff0cdea7551ac0051a5ff
samkc-0 <sam.k.c.0@icloud.com>
2024-06-23 16:50:18 +1200

    refactor lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,45 @@
+local lsps = {
+	"lua_ls",
+	"pyright",
+	"cssls",
+	"clangd",
+	"eslint",
+	"gopls",
+	"html",
+	"tailwindcss",
+	"tsserver",
+	"lemminx",
+	"rust_analyzer",
+	"sqlls",
+  "csharp-language-server",
+}
+
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "omnisharp",
-          "cssls",
-          "clangd",
-          "eslint",
-          "gopls",
-          "html",
-          "tailwindcss",
-          "tsserver",
-          "lemminx",
-          "rust_analyzer",
-          "sqlls",
-        }
-    })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.omnisharp.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.tsserver.setup({})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = lsps,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+      for _, lsp_name in ipairs(lsps) do
+        lspconfig[lsp_name].setup({})
+      end
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
