70e0f7c35c13556cd6e6cf97c17c4e65b1fc8c55
samkc-0 <sam.k.c.0@icloud.com>
2024-06-23 17:01:39 +1200

    set treesitter to autoinstall parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,11 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-          ensure_installed = {
-            "lua",
-            "vim",
-            "javascript",
-            "html",
-            "css",
-            "c_sharp",
-            "go",
-            "typescript",
-            "python",
-          },
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      indent = { enable = true },
+    })
+  end,
 }
+
