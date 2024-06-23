8b7f1eb409819c4fe9bf5073ab7d489b40818780
samkc-0 <sam.k.c.0@icloud.com>
2024-06-23 20:34:55 +1200

    enable autocompletions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,12 +17,14 @@ local lsps = {
 return {
 	{
 		"williamboman/mason.nvim",
+    lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+    lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = lsps,
@@ -31,11 +33,15 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
 		config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			for _, lsp_name in ipairs(lsps) do
-				lspconfig[lsp_name].setup({})
+				lspconfig[lsp_name].setup({
+          capabilities = capabilities,
+        })
 			end
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
