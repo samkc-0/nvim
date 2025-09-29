25c50a24fa687d95171b173bddf3ecdeaf41e75a
samkc-0 <sam.k.c.0@icloud.com>
2025-09-29 15:27:48 +0100

    disable relativenumber

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.clipboard:append("unnamed")
 vim.g.mapleader = " "
